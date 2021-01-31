f40457afd93ff3f3c384fd9004f7bc17c3fa7a57
Nourhan Mohamed <nourhan.abdeltawab@gmail.com>
2021-02-01 00:30:23 +0200

    added kubernetes run script

--- run_kubernetes.sh
+++ run_kubernetes.sh
@@ -4,15 +4,18 @@
 
 # Step 1:
 # This is your Docker ID/path
-# dockerpath=<>
+dockerpath="mohamn9/ml_devops"
 
 # Step 2
 # Run the Docker Hub container with kubernetes
-
+kubectl run ml-devops-pod\
+    --image=$dockerpath\
+    --port=80 --labels app=ml_devops_pod --image-pull-policy='IfNotPresent'
 
 # Step 3:
 # List kubernetes pods
+kubectl get pods
 
 # Step 4:
 # Forward the container port to a host
-
+kubectl port-forward ml-devops-pod 8000:80
